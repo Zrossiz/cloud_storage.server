148151593c430160c9c1b60661f9049e2337fd26
Zrossiz <soon.pluxury@mail.ru>
2024-06-06 00:05:22 +0300

    feat: added create user route

--- user/models.py
+++ user/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField()
-    email = models.CharField(blank=True, null=True)
+    username = models.CharField(unique=True)
+    email = models.CharField(blank=True, null=True, unique=True)
     password = models.CharField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- user/views.py
+++ user/views.py
@@ -1,10 +1,46 @@
 from django.http import HttpResponse
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+import bcrypt
+
+from .serializers import UserSerializer
+from .models import User
 
 # Create your views here.
 class UserView():
 
+    @api_view(['POST'])
     def sign_up(request):
-        return HttpResponse('sign up route')
+        try:
+            body = request.data
+            existUser = User.objects.filter(username=body['username']).exists()
+            
+            if existUser:
+                return Response({
+                'success': False,
+                'message': 'username or email already taken',
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+
+            password = request.data["password"].encode('utf-8')
+            hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+            request.data['password'] = hashed_password.decode('utf-8')
+            serializer = UserSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+
+            return Response({
+                'success': True,
+                'data': serializer.data,
+            }, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            print(e)
+            return Response({
+                'success': False,
+                'message': 'Server error',
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
     def login(request):
         return HttpResponse('login route')
