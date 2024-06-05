3fc9d99107a38e3e3933fda1d1ccc86b144fe410
Zrossiz <soon.pluxury@mail.ru>
2024-06-05 23:15:40 +0300

    feat: added user and session model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField()
+    email = models.CharField(blank=True, null=True)
+    password = models.CharField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "user"
+
+class UserSession(models.Model):
+    token = models.CharField
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exp = models.BigIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "user_session"
