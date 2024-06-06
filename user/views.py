02dcd60243fe077492482a5cc0480a01b45cb7ef
Zrossiz <soon.pluxury@mail.ru>
2024-06-06 20:01:48 +0300

    feat: added login route

--- user/views.py
+++ user/views.py
@@ -6,14 +6,17 @@ import datetime
 import dateutil.relativedelta
 import uuid
 from django.http import HttpResponse
-from .models import User
+from .models import User, UserSession
 from .serializers import UserSerializer, UserSessionSerializer
 
 class UserView(APIView):
 
     def post(self, request, *args, **kwargs):
+
         if 'sign-up' in request.path:
             return self.sign_up(request)
+        if 'login' in request.path:
+            return self.login(request)
         
         return Response({'message': 'Invalid endpoint'}, status=status.HTTP_404_NOT_FOUND)
 
@@ -34,7 +37,7 @@ class UserView(APIView):
             serializer = UserSerializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             serializer.save()
-            session = self._generate_token(serializer.data['id'])
+            session = self._get_token(serializer.data['id'])
 
             response =  Response({
                 'success': True,
@@ -59,7 +62,51 @@ class UserView(APIView):
             }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def login(self, request):
-        return HttpResponse('login route')
+        try:
+            data = request.data
+            username = data['username']
+            password = data['password'].encode('utf-8')
+
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return Response({
+                    'success': False,
+                    'message': 'username or password invalid',
+                }, status=status.HTTP_400_BAD_REQUEST)
+            
+            is_matched_passwords = bcrypt.checkpw(password, user.password.encode('utf-8'))
+
+            if not is_matched_passwords:
+                return Response({
+                    'success': False,
+                    'message': 'username or password invalid',
+                }, status=status.HTTP_400_BAD_REQUEST)
+            
+
+            serializer = UserSerializer(user)
+            session = self._get_token(serializer.data['id'])
+
+            response =  Response({
+                'success': True,
+                'data': serializer.data,
+            }, status=status.HTTP_200_OK)
+
+            response.set_cookie(
+                key='session',
+                value=session,
+                httponly=True,
+                samesite='Strict',
+                secure=True
+            )
+
+            return response
+        except Exception as e:
+            print(e)
+            return Response({
+                'success': False,
+                'message': 'Server error',
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def logout(self, request):
         return HttpResponse('logout route')
@@ -70,7 +117,17 @@ class UserView(APIView):
     def delete(self, request):
         return HttpResponse('delete user route')
 
-    def _generate_token(self, user_id):
+    def _get_token(self, user_id):
+        exist_session = UserSession.objects.filter(user_id=user_id).first()
+
+        if exist_session:
+            exp_datetime = datetime.datetime.fromtimestamp(exist_session.exp / 1000.0, datetime.timezone.utc)
+
+            if exp_datetime > datetime.datetime.now(datetime.timezone.utc):
+                return exist_session.token
+            else:
+                exist_session.delete()
+
         cur_time = datetime.datetime.now()
         next_month = cur_time + dateutil.relativedelta.relativedelta(months=1)
         diff_time = next_month - cur_time
