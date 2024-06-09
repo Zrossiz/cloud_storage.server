60c7646fbb984affa5df74bbb7032e83c209c8ca
Zrossiz <soon.pluxury@mail.ru>
2024-06-09 13:47:55 +0300

    feat: added docker redis

--- core/settings.py
+++ core/settings.py
@@ -131,3 +131,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_ENGINE = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6369/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        }
+    }
+}
