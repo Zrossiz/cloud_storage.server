d882b6bd528ffcf0e98cac8ca098febf6cb77c57
Zrossiz <soon.pluxury@mail.ru>
2024-06-14 23:00:45 +0300

    refactor: routes

--- internal/transport/rest/handler/user/handler.go
+++ internal/transport/rest/handler/user/handler.go
@@ -8,6 +8,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -16,64 +17,78 @@ import (
 )
 
 func UserHandler(db *gorm.DB, redis *redis.Client) http.HandlerFunc {
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			response.SendError(w, http.StatusMethodNotAllowed, "Invalid request method")
-			return
+		if r.Method == http.MethodPost && strings.Contains(r.URL.String(), "register") {
+			create(w, r, db, redis)
 		}
-
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			response.SendError(w, http.StatusInternalServerError, "Error reading request body")
-			return
+		if r.Method == http.MethodPost && strings.Contains(r.URL.String(), "login") {
+			login(w, r, db, redis)
 		}
-		defer r.Body.Close()
-
-		var user models.User
-		if err := json.Unmarshal(body, &user); err != nil {
-			response.SendError(w, http.StatusBadRequest, "Error parsing JSON")
-			return
-		}
-
-		var existingUser models.User
-		if err := db.Where("email = ?", user.Email).First(&existingUser).Error; err == nil {
-			response.SendError(w, http.StatusConflict, "Email already in use")
-			return
-		}
-		if err := db.Where("name = ?", user.Name).First(&existingUser).Error; err == nil {
-			response.SendError(w, http.StatusConflict, "Username already in use")
-			return
-		}
-
-		if err := user.SetPassword(user.Password); err != nil {
-			response.SendError(w, http.StatusInternalServerError, "Error hashing password")
-			return
-		}
-
-		if err := db.Create(&user).Error; err != nil {
-			response.SendError(w, http.StatusInternalServerError, "Error saving user")
-			return
-		}
-		token := uuid.New().String()
-		redisUserId := fmt.Sprintf("%d", user.ID)
-		redisSession := fmt.Sprintf("%s", token)
-		ctx := context.Background()
-
-		err = redis.Set(ctx, redisUserId, redisSession, 10*time.Second).Err()
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			response.SendError(w, http.StatusInternalServerError, "Error saving session")
-			return
-		}
-
-		cookie := http.Cookie{
-			Name:    "session",
-			Value:   token,
-			Expires: time.Now().Add(10 * time.Second),
-			Path:    "/",
-		}
-		http.SetCookie(w, &cookie)
-
-		response.SendData(w, http.StatusCreated, user)
 	}
+}
+
+func create(w http.ResponseWriter, r *http.Request, db *gorm.DB, redis *redis.Client) {
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		response.SendError(w, http.StatusInternalServerError, "Error reading request body")
+		return
+	}
+	defer r.Body.Close()
+
+	var user models.User
+	if err := json.Unmarshal(body, &user); err != nil {
+		response.SendError(w, http.StatusBadRequest, "Error parsing JSON")
+		return
+	}
+
+	var existingUser models.User
+	if err := db.Where("email = ?", user.Email).First(&existingUser).Error; err == nil {
+		response.SendError(w, http.StatusConflict, "Email already in use")
+		return
+	}
+	if err := db.Where("name = ?", user.Name).First(&existingUser).Error; err == nil {
+		response.SendError(w, http.StatusConflict, "Username already in use")
+		return
+	}
+
+	if err := user.SetPassword(user.Password); err != nil {
+		response.SendError(w, http.StatusInternalServerError, "Error hashing password")
+		return
+	}
+
+	if err := db.Create(&user).Error; err != nil {
+		response.SendError(w, http.StatusInternalServerError, "Error saving user")
+		return
+	}
+
+	token := uuid.New().String()
+	redisUserId := fmt.Sprintf("%d", user.ID)
+	ctx := context.Background()
+
+	err = redis.Set(ctx, redisUserId, token, 10*time.Second).Err()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		response.SendError(w, http.StatusInternalServerError, "Error saving session")
+		return
+	}
+
+	cookie := http.Cookie{
+		Name:    "session",
+		Value:   token,
+		Expires: time.Now().Add(10 * time.Second),
+		Path:    "/",
+	}
+	http.SetCookie(w, &cookie)
+
+	response.SendData(w, http.StatusCreated, user)
+}
+
+func login(w http.ResponseWriter, r *http.Request, db *gorm.DB, redis *redis.Client) {
+	// body, err := io.ReadAll(r.Body)
+	// if err != nil {
+	// 	response.SendError(w, http.StatusInternalServerError, "Error reading request body")
+	// }
+	// defer r.Body.Close()
+
 }
--- internal/transport/rest/response/response.go
+++ internal/transport/rest/response/response.go
@@ -13,12 +13,14 @@ type Response struct {
 
 func SendError(w http.ResponseWriter, status int, message string) {
 	response := Response{Success: false, Error: message}
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(response)
 }
 
 func SendData(w http.ResponseWriter, status int, data interface{}) {
 	response := Response{Success: true, Data: data}
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(response)
 }
