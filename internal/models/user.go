35465f7fca69e26415d54dac3ea82ffb34d1d607
Zrossiz <soon.pluxury@mail.ru>
2024-06-14 13:37:27 +0300

    refactor: response format

--- internal/models/user.go
+++ internal/models/user.go
@@ -3,15 +3,30 @@ package models
 import (
 	"time"
 
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
 type User struct {
 	ID        uint           `gorm:"primaryKey" json:"id"`
-	Name      string         `gorm:"size:100;not null" json:"name"`
-	Email     string         `gorm:"size:100;unique;not null" json:"email"`
-	Password  string         `gorm:"not null" json:"password"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `gorm:"index" json:"deleted_at,omitempty"`
+	Name      string         `gorm:"size:100;not null;unique" json:"name"`
+	Email     string         `gorm:"size:100;not null;unique" json:"email"`
+	Password  string         `gorm:"not null" json:"password"`
+}
+
+func (user *User) SetPassword(password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	user.Password = string(hashedPassword)
+	return nil
+}
+
+func (user *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	return err == nil
 }
--- internal/transport/rest/handler/user/handler.go
+++ internal/transport/rest/handler/user/handler.go
@@ -2,76 +2,54 @@ package user
 
 import (
 	"cloudStorage/internal/models"
+	"cloudStorage/internal/transport/rest/response"
 	"encoding/json"
 	"io"
 	"net/http"
 
-	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
-
-type Response struct {
-	Success bool   `json:"success"`
-	Error   string `json:"error,omitempty"`
-}
 
 func UserHandler(db *gorm.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodPost {
-			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+			response.SendError(w, http.StatusMethodNotAllowed, "Invalid request method")
 			return
 		}
 
 		body, err := io.ReadAll(r.Body)
 		if err != nil {
-			http.Error(w, "Error reading request body", http.StatusInternalServerError)
+			response.SendError(w, http.StatusInternalServerError, "Error reading request body")
 			return
 		}
 		defer r.Body.Close()
 
 		var user models.User
-
 		if err := json.Unmarshal(body, &user); err != nil {
-			http.Error(w, "Error parsing JSON", http.StatusBadRequest)
+			response.SendError(w, http.StatusBadRequest, "Error parsing JSON")
 			return
 		}
 
-		var existingUserByEmail models.User
-		if err := db.Where("email = ?", user.Email).First(&existingUserByEmail).Error; err == nil {
-			response := Response{Success: false, Error: "Email already in use"}
-			w.WriteHeader(http.StatusConflict)
-			json.NewEncoder(w).Encode(response)
+		var existingUser models.User
+		if err := db.Where("email = ?", user.Email).First(&existingUser).Error; err == nil {
+			response.SendError(w, http.StatusConflict, "Email already in use")
+			return
+		}
+		if err := db.Where("name = ?", user.Name).First(&existingUser).Error; err == nil {
+			response.SendError(w, http.StatusConflict, "Username already in use")
 			return
 		}
 
-		var existingUserByUsername models.User
-		if err := db.Where("name = ?", user.Name).First(&existingUserByUsername).Error; err == nil {
-			response := Response{Success: false, Error: "Username already in use"}
-			w.WriteHeader(http.StatusConflict)
-			json.NewEncoder(w).Encode(response)
+		if err := user.SetPassword(user.Password); err != nil {
+			response.SendError(w, http.StatusInternalServerError, "Error hashing password")
 			return
 		}
-
-		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
-		if err != nil {
-			http.Error(w, "Error hashing password", http.StatusInternalServerError)
-		}
-
-		user.Password = string(hashedPassword)
 
 		if err := db.Create(&user).Error; err != nil {
-			http.Error(w, "Error saving user", http.StatusInternalServerError)
+			response.SendError(w, http.StatusInternalServerError, "Error saving user")
 			return
 		}
 
-		userJSON, err := json.Marshal(user)
-		if err != nil {
-			http.Error(w, "Error generating JSON", http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusCreated)
-		w.Write(userJSON)
+		response.SendData(w, http.StatusCreated, user)
 	}
 }
